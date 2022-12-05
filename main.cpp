5219c2f7f415a303a2355d74c77e87e4e412afeb
rudgks9326 <44034160+rudgks9326@users.noreply.github.com>
2022-12-05 16:51:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,95 @@
 #include <iostream>
-#include <vector>
+#include <map>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include "helper.h"
 using namespace std;
+void GetDataFromCSV(string filepath, map<string, CSVData> &library ) {
+	ifstream inFile(filepath);
+	if (inFile.is_open()) {
+		string lineFromFile;
+		getline(inFile, lineFromFile);
 
-vector<int> temp(7);
-vector<int> vec = { 38, 27, 43, 9, 3, 82, 10 };
+		while (getline(inFile, lineFromFile)) {
+			istringstream stream(lineFromFile);
+    			string  bookId, title,author="";
+    			string rating, pages,numRatings, likedPercent,bbeScore,bbeVotes,price="";
+    	string  bookId, title,author="";
+    	string rating, pages,numRatings, likedPercent,bbeScore,bbeVotes,price="";
 
-void merge(int start, int end) {
-    int mid = (start + end) / 2;
-    int left_index = start;
-    int right_index = mid + 1;
-    int k = start;
+			getline(stream, bookId, ',');
+			getline(stream, title, ',');
+			getline(stream, author, ',');
+			getline(stream, rating, ',');
+			getline(stream, pages, ',');
+			getline(stream, numRatings, ',');
+			getline(stream, likedPercent, ',');
+			getline(stream, bbeScore, ',');
+			getline(stream, bbeVotes, ',');
+			getline(stream, price, ',');
+			CSVData data;
+			data.bookId = bookId;
+			data.title = title;
+			data.author = author;
+			data.rating = stod(rating);
+			 data.bookId = bookId;
+			 data.title = title;
+			 data.author = author;
+			 data.rating = stod(rating);
+			data.pages = stoi(pages);
+			data.numRatings = stoi(numRatings);
+			data.likedPercent = stoi(likedPercent);
+			data.bbeScore = stoi(bbeScore);
+			data.bbeVotes = stoi(bbeVotes);
+			data.price = stod(price);
+			library[title] = data;
+		}
 
-    while (k <= end) {
-        if (left_index > mid) {
-            temp[k++] = vec[right_index++];
-            continue;
-        }
-        if (right_index > end) {
-            temp[k++] = vec[left_index++];
-            continue;
-        }
-        if (vec[left_index] <= vec[right_index])
-            temp[k++] = vec[left_index++];
-        else
-            temp[k++] = vec[right_index++];
-    }
-    for (int i = start; i <= end; i++) vec[i] = temp[i];
+	}
 }
 
-void merge_sort(int start, int end) {
-    if (start >= end) return;
-    int mid = (start + end) / 2;
-    merge_sort(start, mid);
-    merge_sort(mid + 1, end);
-    merge(start, end);
-}
+int main()
+{
+	map<string, CSVData> library;
 
-void print() { // print all of the element in the vector
-    for (const int& n : vec) cout << n << ' ';
-}
+	GetDataFromCSV("FinalCleanedLibrary.csv",library);
+	cout << "done";	
+	int selection;
+	int range;
+	while(selection!=5){
+		cout << "How would you like to find your book?"<<endl;
+		cout << "1. Sort by page number"<<endl;
+		cout << "2. Sort by rating"<<endl;
+		cout << "3. Sort by price"<<endl;
+		cout << "4. Sort by liked percentage"<<endl;
+		cout << "5. Exit Library"<<endl;
+		cin >>selection;
 
-int main() {
-    merge_sort(0, 6);
-    // print the result
-    print();
+	if(selection ==1 ){
+		cout << "How many results would you like to see?"<<endl;
+		cin >> range;
+	}
+	else if (selection ==2){
+		cout << "How many results would you like to see?"<<endl;
+		cin >> range;
+	}
+	else if (selection ==3){
+		cout << "How many results would you like to see?"<<endl;
+		cin >> range;
+	}
+	else if (selection ==4){
+		cout << "How many results would you like to see?"<<endl;
+		cin >> range;
+	}
+	else if (selection ==5){
+		break;
+	}
+	else
+	cout << "Please make a valid selection"<<endl;
 
-    return 0;
+	}
+	cout << "Thank you for stopping by, I hope you enjoyed our slection and found a good book!";
+
 }
