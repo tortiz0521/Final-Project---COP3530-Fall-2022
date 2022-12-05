4a44986dffd4e3da0bcb52612e21a2337af9f0dc
rudgks9326 <44034160+rudgks9326@users.noreply.github.com>
2022-12-05 13:53:47 -0500

    Update mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include <map>
+#include <chrono>
 
 using namespace std;
 
@@ -36,7 +37,6 @@ void MergeSortedIntervals(vector<pair<string, double>>& v, int start, int mid, i
 
 	for (int i = start; i <= end; i++)
 		v[i] = temp[i - start];
-
 }
 
 // the MergeSort function Sorts the array in the range [start to end] in v using merge sort algorithm
@@ -60,13 +60,13 @@ int main() {
 	books.insert(std::pair<string, double>("Bugs Life", 4.7));
 	books.insert(std::pair<string, double>("Naruto", 3.9));
 	// for check same value
-	books.insert(std::pair<string, double>("book1", 4.5));
-	books.insert(std::pair<string, double>("book2", 4.3));
-	books.insert(std::pair<string, double>("book3", 4.8));
-	books.insert(std::pair<string, double>("book4", 4.2));
-	books.insert(std::pair<string, double>("book5", 5.0));
-	books.insert(std::pair<string, double>("book6", 4.7));
-	books.insert(std::pair<string, double>("book7", 3.9));
+	books.insert(std::pair<string, double>("League of Legend", 4.5));
+	books.insert(std::pair<string, double>("OverWatch", 4.3));
+	books.insert(std::pair<string, double>("Star Craft", 4.8));
+	books.insert(std::pair<string, double>("War Craft", 4.2));
+	books.insert(std::pair<string, double>("Counter Strike", 5.0));
+	books.insert(std::pair<string, double>("Battle Field", 4.7));
+	books.insert(std::pair<string, double>("Steam", 3.9));
 	cout << "book size is: " << books.size() << endl;
 	cout << endl << "Default map Order is: " << endl;
 	vector<pair<string, double>> v;
@@ -81,7 +81,10 @@ int main() {
 	for (auto& it : v) {
 		cout << it.first << ' ' << it.second << endl;
 	}
+	auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
 	MergeSort(v, 0, n - 1);
+	auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+	auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
 	// print the ascending order
 	cout << endl << "Ascending Order: " << endl;
 	for (auto& it : v) {
@@ -92,4 +95,6 @@ int main() {
 	for (int i = n - 1; i >= 0; i--) {
 		cout << v[i].first << ' ' << v[i].second << endl;
 	}
+	cout << endl;
+	cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
 }
