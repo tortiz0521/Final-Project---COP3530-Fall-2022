3aee30288aa8b05bade29868cd74451fda393978
tortiz0521 <92881309+tortiz0521@users.noreply.github.com>
2022-12-05 23:50:16 -0500

    Add files via upload
    The fully implemented version!

--- RadixSort.h
+++ RadixSort.h
@@ -7,50 +7,75 @@
 using namespace std;
 
 
-int findMax(const vector<int> data)
+int findMax(const vector<pair<string, int>> data)
 {
-	int max = data[0];
+	int max = data[0].second;
 	for (int i = 0; i < data.size(); i++) {
-		if (data[i] > max) {
-			max = data[i];
+		if (data[i].second > max) {
+			max = data[i].second;
 		}
 	}
 
 	return max;
 }
 
-void countingSort(vector<int>& data, int digit)
+void countingSort(vector<pair<string, int>>& data, int digit)
 {
-	vector<int> out(data.size());
+	vector<pair<string, int>> out(data.size());
 	int helpArr[10] = { 0 };
 
 	for (int i = 0; i < data.size(); i++) {
-		int searchVal = (data[i] / digit) % 10;
-		helpArr[searchVal]++;
+		int sVal = (data[i].second / digit) % 10;
+		helpArr[sVal]++;
 	}
 
 	for (int i = 1; i < 10; i++) {
 		helpArr[i] += helpArr[i - 1];
 	}
 
-	for (int i = 0; i < data.size(); i++) {
-		int searchVal = (data[i] / digit) % 10;
-		out[helpArr[searchVal - 1]] = data[i];
-		helpArr[searchVal]--;
+	for (int i = data.size() - 1; i >= 0; i--) {
+		int sVal = (data[i].second / digit) % 10;
+		out[helpArr[sVal] - 1].first = data[i].first;
+		out[helpArr[sVal] - 1].second = data[i].second;
+		helpArr[sVal]--;
 	}
 
 	for (int i = 0; i < out.size(); i++) {
-		data[i] = out[i];
+		data[i].first = out[i].first;
+		data[i].second = out[i].second;
 	}
 }
 
-void radixSort(vector<int>& data)
+vector<pair<string, int>> dedecimal(vector<pair<string, double>> data)
 {
-	int max = findMax(data);
+	vector<pair<string, int>> d;
+	for (int i = 0; i < data.size(); i++) {
+		d.push_back(make_pair(data[i].first, data[i].second * 100));
+	}
+
+	return d;
+}
+
+vector<pair<string, double>> redecimal(vector<pair<string, int>> data)
+{
+	vector<pair<string, double>> d;
+	for (int i = 0; i < data.size(); i++) {
+		d.push_back(make_pair(data[i].first, (double)data[i].second / 100.0));
+	}
+
+	return d;
+}
+
+void radixSort(vector<pair<string, double>>& data)
+{
+	vector<pair<string, int>> de = dedecimal(data);
+	int max = findMax(de);
 
 	for (int d = 1; max / d > 0; d *= 10) {
-		countingSort(data, d);
+		countingSort(de, d);
 	}
+
+	data = redecimal(de);
 }
 
 #endif
