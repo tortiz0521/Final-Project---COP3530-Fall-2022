956d886a82c402eb53c64fe8ec729a05c8d8ea41
rudgks9326 <44034160+rudgks9326@users.noreply.github.com>
2022-12-06 15:27:17 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include "helper.h"
 #include "mergeSort.h"
+#include "radixSort.h"
 
 using namespace std;
 
@@ -74,33 +75,55 @@ vector<pair<string, int>> createListInt(vector<pair<string, int>>& list, string 
 	return list;
 
 }
-void intSelect (int range, vector<pair<string, int>> list, map<string, CSVData>& library, string type){
-			list = createListInt(list, type, library);
-			int size = list.size();
-			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
-			mergeSort_int(list, 0, size - 1);
-			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
-			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
-			cout << endl << "Descending Order: " << endl;
-			for (int i = size - 1; i >= size - range; i--) {
-				cout << list[i].first << ' ' << list[i].second << endl;
-			}
-			cout << endl;
-			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+void intSelect(int range, vector<pair<string, int>> list, map<string, CSVData>& library, string type) {
+	list = createListInt(list, type, library);
+	int size = list.size();
+	vector<pair<string, int>> forMerge = list;
+	vector<pair<string, int>> forRadix = list;
+	// for caclulate the execute time for merge sort 
+	auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+	mergeSort_int(forMerge, 0, size - 1);
+	auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+	auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count();
+	// for calculate the execute time for radix sort
+	auto start2 = chrono::high_resolution_clock::now(); // Count to start the time on here
+	radixSort_int(forRadix);
+	auto end2 = chrono::high_resolution_clock::now(); // stop the time on here
+	auto time2 = chrono::duration_cast<chrono::microseconds>(end2 - start2).count();
+	cout << endl << "Descending Order: " << endl;
+	for (int i = size - 1; i >= size - range; i--) {
+		cout << forMerge[i].first << ' ' << forMerge[i].second << endl;
+	}
+	cout << endl;
+	cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+	cout << endl;
+	cout << "Time taken by Radix Sort: " << time2 << " microseconds." << endl; // print the time to execute
+	cout << endl;
 }
-void dblSelect (int range, vector<pair<string, double>> list, map<string, CSVData>& library, string type){
-			list = createListDouble(list,type, library);
-			int size = list.size();
-			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
-			mergeSort_double(list, 0, size - 1);
-			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
-			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
-			cout << endl << "Descending Order: " << endl;
-			for (int i = size - 1; i >= size - range; i--) {
-				cout << list[i].first << ' ' << fixed << showpoint << setprecision(2) << list[i].second << endl;
-			}
-			cout << endl;
-			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+void dblSelect(int range, vector<pair<string, double>> list, map<string, CSVData>& library, string type) {
+	list = createListDouble(list, type, library);
+	int size = list.size();
+	vector<pair<string, double>> forMerge = list;
+	vector<pair<string, double>> forRadix = list;
+	auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+	mergeSort_double(forMerge, 0, size - 1);
+	auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+	auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count();
+	
+	// for calculate the execute time for radix sort
+	auto start2 = chrono::high_resolution_clock::now(); // Count to start the time on here
+	radixSort_double(forRadix);
+	auto end2 = chrono::high_resolution_clock::now(); // stop the time on here
+	auto time2 = chrono::duration_cast<chrono::microseconds>(end2 - start2).count();
+	cout << endl << "Descending Order: " << endl;
+	for (int i = size - 1; i >= size - range; i--) {
+		cout << forMerge[i].first << ' ' << fixed << showpoint << setprecision(2) << forMerge[i].second << endl;
+	}
+	cout << endl;
+	cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+	cout << endl;
+	cout << "Time taken by Merge Sort: " << time2 << " microseconds." << endl; // print the time to execute
+	cout << endl;
 }
 int main()
 {
@@ -109,7 +132,7 @@ int main()
 	GetDataFromCSV("FinalCleanedLibrary.csv", library);
 	int selection = 0;
 	int range;
-	
+
 
 	while (true) {
 
@@ -127,46 +150,46 @@ int main()
 			cout << "How many results would you like to see?" << endl;
 			cin >> range;
 			vector<pair<string, int>> listPages;
-			intSelect(range,listPages,library, "pages");
+			intSelect(range, listPages, library, "pages");
 		}
 		else if (selection == 2) {
 			cout << "How many results would you like to see?" << endl;
 			cin >> range;
-			vector<pair<string, double>> listRating;	
-			dblSelect(range,listRating,library, "rating");
+			vector<pair<string, double>> listRating;
+			dblSelect(range, listRating, library, "rating");
 		}
-		else if (selection == 3) {	
+		else if (selection == 3) {
 			cout << "How many results would you like to see?" << endl;
-			cin >> range;	
+			cin >> range;
 			vector<pair<string, double>> listPrice;
-			dblSelect(range,listPrice,library, "price");
+			dblSelect(range, listPrice, library, "price");
 		}
 		else if (selection == 4) {
 			cout << "How many results would you like to see?" << endl;
 			cin >> range;
 			vector<pair<string, int>> listlikedPercent;
-			intSelect(range,listlikedPercent,library,"likedPercent");
+			intSelect(range, listlikedPercent, library, "likedPercent");
 		}
-		else if (selection == 5){
-			cout<< "What is the name of the book you are searching for?"<<endl;
+		else if (selection == 5) {
+			cout << "What is the name of the book you are searching for?" << endl;
 			string book;
 			getline(cin, book);
-			if (library[book].rating == 0){
-			cout<< "This is not a valid book in our Library, please start over." <<endl;
-			cout<<endl;
-			continue;
+			if (library[book].rating == 0) {
+				cout << "This is not a valid book in our Library, please start over." << endl;
+				cout << endl;
+				continue;
 			}
-			cout << "Author: "<<library[book].author<< ", Rating: " << library[book].rating << ", \nNumber of Pages: "<<
-			library[book].pages<<", Number of Ratings: "<<library[book].numRatings<< ", \nPercentage Liked: "<<
-			library[book].likedPercent<< ", Price of Book: "<< library[book].price<< ", \nBest Books Ever Score: "<<
-			library[book].bbeScore <<", Best Books Ever Votes: "<<library[book].bbeVotes <<endl;
+			cout << "Author: " << library[book].author << ", Rating: " << library[book].rating << ", \nNumber of Pages: " <<
+				library[book].pages << ", Number of Ratings: " << library[book].numRatings << ", \nPercentage Liked: " <<
+				library[book].likedPercent << ", Price of Book: " << library[book].price << ", \nBest Books Ever Score: " <<
+				library[book].bbeScore << ", Best Books Ever Votes: " << library[book].bbeVotes << endl;
 		}
 		else if (selection == 6) {
 			break;
 		}
 		else
 			cout << "Please make a valid selection" << endl;
-			cout <<endl;
+		cout << endl;
 
 	}
 	cout << "Thank you for stopping by, I hope you enjoyed our slection and found a good book!";
