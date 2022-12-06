1eece98eaae3526882276ee4aa3d059c5f38be57
rudgks9326 <44034160+rudgks9326@users.noreply.github.com>
2022-12-06 00:41:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 #include <iomanip>
 #include <sstream>
 #include <fstream>
-#include "helper.h"
 #include <utility>
 #include<vector>
+#include <chrono>
+#include "helper.h"
+#include "mergeSort.h"
+
 using namespace std;
 
-void GetDataFromCSV(string filepath, map<string, CSVData> &library ) {
+void GetDataFromCSV(string filepath, map<string, CSVData>& library) {
 	ifstream inFile(filepath);
 
 	if (inFile.is_open()) {
@@ -18,9 +21,9 @@ void GetDataFromCSV(string filepath, map<string, CSVData> &library ) {
 
 		while (getline(inFile, lineFromFile)) {
 			istringstream stream(lineFromFile);
-    	string  bookId, title,author="";
-    	string rating, pages,numRatings, likedPercent,bbeScore,bbeVotes,price="";
-			
+			string  bookId, title, author = "";
+			string rating, pages, numRatings, likedPercent, bbeScore, bbeVotes, price = "";
+
 			getline(stream, bookId, ',');
 			getline(stream, title, ',');
 			getline(stream, author, ',');
@@ -32,10 +35,10 @@ void GetDataFromCSV(string filepath, map<string, CSVData> &library ) {
 			getline(stream, bbeVotes, ',');
 			getline(stream, price, ',');
 			CSVData data;
-			 data.bookId = bookId;
-			 data.title = title;
-			 data.author = author;
-			 data.rating = stod(rating);
+			data.bookId = bookId;
+			data.title = title;
+			data.author = author;
+			data.rating = stod(rating);
 			data.pages = stoi(pages);
 			data.numRatings = stoi(numRatings);
 			data.likedPercent = stoi(likedPercent);
@@ -47,27 +50,26 @@ void GetDataFromCSV(string filepath, map<string, CSVData> &library ) {
 
 	}
 }
-vector<pair<string, double>> createListDouble(vector<pair<string, double>> &list, string value,const map<string, CSVData> &library ){
-	for (const auto &p : library)
-{
-	if (value == "rating")
-    list.push_back(std::make_pair(p.first, p.second.rating));    
-	else if (value == "price")
-	list.push_back(std::make_pair(p.first, p.second.price));   
-}
+vector<pair<string, double>> createListDouble(vector<pair<string, double>>& list, string value, const map<string, CSVData>& library) {
+	for (const auto& p : library)
+	{
+		if (value == "rating")
+			list.push_back(std::make_pair(p.first, p.second.rating));
+		else if (value == "price")
+			list.push_back(std::make_pair(p.first, p.second.price));
+	}
 	return list;
 
 }
-vector<pair<string, int>> createListInt(vector<pair<string, int>> &list, string value,map<string, CSVData> &library ){
+vector<pair<string, int>> createListInt(vector<pair<string, int>>& list, string value, map<string, CSVData>& library) {
 	for (auto it = library.begin(); it != library.end(); it++)
-{	
-	if (value == "pages"){
-	list.push_back(std::make_pair(it->first, it->second.pages));  
-	cout<< "adding"<<endl;    
+	{
+		if (value == "pages") {
+			list.push_back(std::make_pair(it->first, it->second.pages));
+		}
+		else if (value == "likedPercent")
+			list.push_back(std::make_pair(it->first, it->second.likedPercent));
 	}
-	else if (value == "likedPercent")
-	list.push_back(std::make_pair(it->first, it->second.likedPercent)); 
-}
 	return list;
 
 }
@@ -75,55 +77,96 @@ int main()
 {
 	map<string, CSVData> library;
 
-	GetDataFromCSV("FinalCleanedLibrary.csv",library);
-	int selection;
+	GetDataFromCSV("FinalCleanedLibrary.csv", library);
+	int selection = 0;
 	int range;
 
+	int sizeRating;
+	int sizePrice;
+	int sizePage;
+	int sizePercent;
+
+	while (selection != 5) {
+
+		cout << "How would you like to find your book?" << endl;
+		cout << "1. Sort by page number" << endl;
+		cout << "2. Sort by rating" << endl;
+		cout << "3. Sort by price" << endl;
+		cout << "4. Sort by liked percentage" << endl;
+		cout << "5. Exit Library" << endl;
+		cin >> selection;
+		cout << "How many results would you like to see?" << endl;
+		cin >> range;
+		if (selection == 1) {
+			vector<pair<string, int>> listPages;
+			listPages = createListInt(listPages, "pages", library);
+			sizePage = listPages.size();
+			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+			mergeSort_int(listPages, 0, sizePage - 1);
+			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
+			cout << endl << "Descending Order: " << endl;
+			for (int i = sizePage - 1; i >= sizePage - range; i--) {
+				cout << listPages[i].first << ' ' << listPages[i].second << endl;
+			}
+			cout << endl;
+			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+		}
+		else if (selection == 2) {
+			vector<pair<string, double>> listRating;
+			listRating = createListDouble(listRating, "rating", library);
+			sizeRating = listRating.size();
+			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+			mergeSort_double(listRating, 0, sizeRating - 1);
+			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
+			cout << endl << "Descending Order: " << endl;
+			for (int i = sizeRating - 1; i >= sizeRating - range; i--) {
+				cout << listRating[i].first << ' ' << listRating[i].second << endl;
+			}
+			cout << endl;
+			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+		}
+		else if (selection == 3) {		
+			vector<pair<string, double>> listPrice;
+			listPrice = createListDouble(listPrice, "price", library);
+			sizePrice = listPrice.size();
+			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+			mergeSort_double(listPrice, 0, sizePrice - 1);
+			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
+			cout << endl << "Descending Order: " << endl;
+			for (int i = sizePrice - 1; i >= sizePrice - range; i--) {
+				cout << listPrice[i].first << ' ' << listPrice[i].second << endl;
+			}
+			cout << endl;
+			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
+
+		}
+		else if (selection == 4) {
+			vector<pair<string, int>> listlikedPercent;
+			listlikedPercent = createListInt(listlikedPercent, "likedPercent", library);
+			sizePercent = listlikedPercent.size();
+			auto start1 = chrono::high_resolution_clock::now(); // Count to start the time on here
+			mergeSort_int(listlikedPercent, 0, sizePercent - 1);
+			auto end1 = chrono::high_resolution_clock::now(); // stop the time on here
+			auto time1 = chrono::duration_cast<chrono::microseconds>(end1 - start1).count(); // Caculate the time 
+			cout << endl << "Descending Order: " << endl;
+			for (int i = sizePercent - 1; i >= sizePercent - range; i--) {
+				cout << listlikedPercent[i].first << ' ' << listlikedPercent[i].second << endl;
+			}
+			cout << endl;
+			cout << "Time taken by Merge Sort: " << time1 << " microseconds." << endl; // print the time to execute
 
 
-	while(selection!=5){
-		
-		cout << "How would you like to find your book?"<<endl;
-		cout << "1. Sort by page number"<<endl;
-		cout << "2. Sort by rating"<<endl;
-		cout << "3. Sort by price"<<endl;
-		cout << "4. Sort by liked percentage"<<endl;
-		cout << "5. Exit Library"<<endl;
-		cin >>selection;
+		}
+		else if (selection == 5) {
+			break;
+		}
+		else
+			cout << "Please make a valid selection" << endl;
+			break;
 
-	if(selection ==1 ){
-		cout << "How many results would you like to see?"<<endl;
-		cin >> range;
-		vector<pair<string, int>> listPages;
-		listPages = createListInt(listPages,"pages",library);
-	
-	}
-	else if (selection ==2){
-		cout << "How many results would you like to see?"<<endl;
-		cin >> range;
-		vector<pair<string, double>> listRating;
-		listRating= createListDouble(listRating,"rating",library);
-		
-	}
-	else if (selection ==3){
-		cout << "How many results would you like to see?"<<endl;
-		cin >> range;
-		vector<pair<string, double>> listPrice;
-		listPrice = createListDouble(listPrice,"price",library);
-	}
-	else if (selection ==4){
-		cout << "How many results would you like to see?"<<endl;
-		cin >> range;
-		vector<pair<string, int>> listlikedPercent;
-		listlikedPercent = createListInt(listlikedPercent,"likedPercent",library);
-	}
-	else if (selection ==5){
-		break;
-	}
-	else
-	cout << "Please make a valid selection"<<endl;
-	
 	}
 	cout << "Thank you for stopping by, I hope you enjoyed our slection and found a good book!";
-	
 }
